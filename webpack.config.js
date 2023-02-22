79882037bb8f9911a3dffe47b44a3ad11f246b9a
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-22 11:02:23 -0300

    Configurando o vue-loader e o babel-loader no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { VueLoader } = require("vue-loader");
 
 module.exports = {
   mode: "development",
@@ -6,5 +7,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "./js/index.js",
+  },
+  plugins: [new VueLoader()],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
