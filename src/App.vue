c9f6cd55b1d1c369968291b558ac6e55b4c52ba9
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-22 11:07:54 -0300

    Criando e editando  App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  mounted() {
+    console.log("A ccontagem inicial e ${this.count}");
+  },
+};
+</script>
+
+<template>
+  <button @click="increment">Count is {{ count }}</button>
+</template>
+
+<style scoped></style>
