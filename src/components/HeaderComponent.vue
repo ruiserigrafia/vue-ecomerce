8b23d8dc9308cc6c9e46ced16817439f4d800833
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-25 17:19:25 -0300

    Criando e editando /src/components/HeaderComponent.vue

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,53 @@
+<script>
+import { LOGO } from "./../img/loaderImg";
+export default {
+  props: { hasFigure: Boolean, hasHgroup: Boolean, hasAside: Boolean },
+  data() {
+    return {
+      hasFigure: this.hasFigure,
+      hasHgroup: this.hasHgroup,
+      hasAside: this.hasAside,
+      figAtrributes: {
+        id: "component-figure",
+      },
+      imgAtrributes: {
+        src: LOGO,
+        id: "logo",
+      },
+    };
+  },
+  beforeMount() {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<template>
+  <header class="container-grid center-centered">
+    <figure v-if="hasFigure" v-bind="figAtrributes">
+      <img v-bind="imgAtrributes" />
+    </figure>
+    <hgroup v-if="hasHgroup"></hgroup>
+    <aside v-if="hasAside">
+      <form action="#"></form>
+    </aside>
+  </header>
+</template>
+
+<style scoped>
+header {
+  grid-template-columns: 20% 60% 20%;
+  width: 95%;
+  /*
+  height: 200px;
+  background-color: aqua;
+  */
+}
+#logo {
+  width: 100%;
+}
+
+#component-figure {
+  width: 80%;
+}
+</style>
