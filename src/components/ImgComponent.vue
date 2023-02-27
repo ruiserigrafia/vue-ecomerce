6292eaceb6c67ea2f97bf800d7c046287fc5dcdf
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-27 01:30:34 -0300

    Criando e editando src/components/ImgComponent.vue

---
+++ src/components/ImgComponent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: { imgAttributes: Object },
+  data() {
+    return {
+      imgBind: this.imgAttributes.attrs,
+    };
+  },
+};
+</script>
+<template>
+  <img v-bind="imgBind" />
+</template>
+<style scoped></style>
