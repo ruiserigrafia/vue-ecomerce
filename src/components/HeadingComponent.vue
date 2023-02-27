ca6d0edb5170702669560ce1d8bb651fce5368aa
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-27 01:33:52 -0300

    Criando e editando src/components/HeadingComponent.vue

---
+++ src/components/HeadingComponent.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    headingAttributes: Object,
+  },
+  data() {
+    return {
+      headingBind: this.headingAttributes.attrs,
+      texts: this.headingAttributes.texts,
+    };
+  },
+};
+</script>
+<template>
+  <h1>{{ texts[0] }}</h1>
+  <h2>{{ texts[1] }}</h2>
+  <h3>{{ texts[2] }}</h3>
+</template>
+<style scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-align: center;
+}
+</style>
