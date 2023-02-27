11e1c7b02b2dffcf7a1c2fdf8209bcb2691d865a
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-27 01:48:18 -0300

    Criando e editando src/components/HgroupComponent.vue

---
+++ src/components/HgroupComponent.vue
@@ -0,0 +1,28 @@
+<script>
+import HeadingComponent from "./HeadingComponent.vue";
+export default {
+  props: {
+    hgroupAttributes: Object,
+  },
+  data() {
+    return {
+      hgroupBind: this.hgroupAttributes.attrs,
+      headingProps: this.hgroupAttributes.heading,
+    };
+  },
+  components: {
+    HeadingComponent,
+  },
+};
+</script>
+
+<template>
+  <hgroup v-bind="hgroupBind">
+    <HeadingComponent :headingAttributes="headingProps" />
+  </hgroup>
+</template>
+<style scoped>
+hgroup {
+  width: 100%;
+}
+</style>
