ec2ee94a85e7e290722ceb13da395d39ae55e36e
ruiserigrafia <ruiserigrafia@gmail.com>
2023-02-27 01:45:42 -0300

    Criando e editando src/components/FigureComponent.vue

---
+++ src/components/FigureComponent.vue
@@ -0,0 +1,28 @@
+<script>
+import ImgComponent from "./ImgComponent.vue";
+export default {
+  props: { figureAttributes: Object },
+  data() {
+    return {
+      figureBind: this.figureAttributes.attrs,
+      imgProps: this.figureAttributes.img,
+      textCaption: this.figureAttributes.figCaption.text,
+    };
+  },
+  components: {
+    ImgComponent,
+  },
+};
+</script>
+<template>
+  <figure v-bind="figureBind">
+    <ImgComponent :imgAttributes="imgProps" />
+    <figcaption>{{ textCaption }}</figcaption>
+  </figure>
+</template>
+<style scoped>
+figcaption {
+  text-align: center;
+  font-size: 1.5em;
+}
+</style>
